b57de5bdff574256785c9f718af439cd51e4ea08
olu <oluegbiaye@gmail.com>
2025-06-23 23:47:02 +0200

    Refactor get_files_info function and add get_file_content function with error handling; enhance tests for both functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,27 +1,54 @@
 from pathlib import Path
 
+MAX_FILE_CONTENT_LENGTH = 9999
+
 
 def get_files_info(working_directory, directory=None):
-    working_dir_path = Path(working_directory)
-    relative_path = Path(directory)
+    working_dir_path = Path(working_directory).resolve()
+
+    if directory is None:
+        directory = "."
+
+    relative_path = Path(directory.lstrip("/"))
+    resolved_target_path = (working_dir_path / relative_path).resolve()
+
+    if not resolved_target_path.is_relative_to(working_dir_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not resolved_target_path.is_dir():
+        return f'Error: "{directory}" is not a directory'
+    contends_of_dir = ""
+    try:
+        for item in resolved_target_path.iterdir():
+            size = item.stat().st_size
+
+            contends_of_dir += (
+                f"- {item.name}: file_size={size} bytes, is_dir={item.is_dir()}\n"
+            )
+
+    except Exception as e:
+        return f"Error: listing files: {e}"
+    else:
+        return contends_of_dir
+
+
+def get_file_content(working_directory, file_path):
+    working_dir_path = Path(working_directory).resolve()
+    relative_path = Path(file_path.lstrip("/"))
 
     resolved_target_path = (working_dir_path / relative_path).resolve()
+
+    if not resolved_target_path.is_relative_to(working_dir_path):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+    if not resolved_target_path.is_file():
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        resolved_target_path.is_relative_to(working_dir_path)
-
-        if not resolved_target_path.exists():
-            return f'Error: Directory "{directory}" does not exist'
-        if not resolved_target_path.is_dir():
-            return f'Error: "{directory}" is not a directory'
-
-        contends_of_dir = ""
-        for item in resolved_target_path.iterdir():
-
-                size = item.stat().st_size
-                contends_of_dir += (
-                    f"- {item.name}: file_size={size} bytes, is_dir={item.is_dir()}\n"
-                )
-    except OSError:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    return contends_of_dir
+        content = resolved_target_path.read_text()
+        if len(content) >= MAX_FILE_CONTENT_LENGTH:
+            content = (
+                content[:MAX_FILE_CONTENT_LENGTH]
+                + f'\n[...File "{file_path}" truncated at 10000 characters]'
+            )
+    except Exception as e:
+        return f"Error: listing files: {e}"
+    else:
+        return content
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content, get_files_info
 
 
 class TestGetFilesInfo(unittest.TestCase):
@@ -13,6 +13,14 @@ class TestGetFilesInfo(unittest.TestCase):
         print(get_files_info("calculator", "/bin"))
         print("\nget_files_info('calculator', '../'):")
         print(get_files_info("calculator", "../"))
+
+    def test_get_file_content(self):
+        print("get_file_content('calculator', 'main.py'):")
+        print(get_file_content("calculator", "main.py"))
+        print("\nget_file_content('calculator', 'pkg/calculator.py'):")
+        print(get_file_content("calculator", "pkg/calculator.py"))
+        print("\nget_file_content('calculator', '/bin/cat'):")
+        print(get_file_content("calculator", "/bin/cat"))
 
 
 if __name__ == "__main__":
